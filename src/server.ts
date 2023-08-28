c322255f308e030ff1f4b4cbfbc18d6fa6b77ca7
mth.dev <matheusdiasmello@icloud.com>
2023-08-27 22:08:26 -0300

    feat: add express initial server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("oi");
+import express from "express";
+import { config } from "dotenv";
+
+const app = express();
+config();
+
+app.use(express.json());
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`server is running in port ${port}`));
